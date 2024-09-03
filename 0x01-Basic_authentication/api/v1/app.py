b5645bd975f92d878d96bdfe8c34df64394a0253
Dopezapha <officialnwaneridaniel@gmail.com>
2024-09-03 12:12:35 -0400

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import Literal
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,19 +20,6 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-
-@app.errorhandler(401)
-def unauthorized(error) -> tuple[str, Literal[401]]:
-    """ Not found handler
-    """
-    return jsonify({"error": "unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> tuple[str, Literal[403]]:
-    """ Not found handler
-    """
-    return jsonify({"error": "forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")

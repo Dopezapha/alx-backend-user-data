defe89752070c42f7054c9fdaf9e525b4eae3a1d
Dopezapha <officialnwaneridaniel@gmail.com>
2024-09-03 11:43:00 -0400

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,11 +20,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> tuple[str, Literal[401]]:
     """ Not found handler
     """
     return jsonify({"error": "unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> tuple[str, Literal[403]]:

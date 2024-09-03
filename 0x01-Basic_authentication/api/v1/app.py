ca539ebadb8a788e06b276c562c5ce5ffc0c6f2a
Dopezapha <officialnwaneridaniel@gmail.com>
2024-09-03 13:23:29 -0400

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,11 +20,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
